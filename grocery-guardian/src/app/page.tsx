7eb47e9991a630c5b488ea8b17849baae4f546b1
Nick-Reichlin <nickreichlin@gmail.com>
2024-03-07 11:00:04 -0500

    Add session access to homepage

--- grocery-guardian/src/app/page.tsx
+++ grocery-guardian/src/app/page.tsx
@@ -1,7 +1,13 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from './api/auth/[...nextauth]/route'
+
 export default async function Home() {
+  const session = await getServerSession(authOptions)
+
+
   return (
     <main>
-        
+        <pre>{JSON.stringify(session)}</pre>
     </main>
   );
 }
