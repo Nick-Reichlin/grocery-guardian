e4adcda82d03a0bb2e7e8b5e2d66c1e2f9c50e1a
Nick-Reichlin <nickreichlin@gmail.com>
2024-03-06 20:11:58 -0500

    Add style for normal page title

--- grocery-guardian/src/app/page.tsx
+++ grocery-guardian/src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="normalPage">
+      <h1 className="signInTitle">Welcome to GroceryGuardian</h1>
     </main>
   );
 }
