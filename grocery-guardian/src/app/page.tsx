4ff31af071cb73f095f6325ce7a175a7dade857d
Nick-Reichlin <nickreichlin@gmail.com>
2024-03-07 10:07:09 -0500

    Add to database schema

--- grocery-guardian/src/app/page.tsx
+++ grocery-guardian/src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="normalPage">
-      <h1 className="signInTitle">Welcome to GroceryGuardian</h1>
-    </main>
+    <body>
+        
+    </body>
   );
 }
