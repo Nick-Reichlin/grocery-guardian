4cafe4daadf107411e78cf191a64b81388c58916
Nick-Reichlin <nickreichlin@gmail.com>
2024-03-08 15:10:57 -0500

    Add styling to form and register page

--- grocery-guardian/src/app/register/form.tsx
+++ grocery-guardian/src/app/register/form.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import React from "react"
 
 export const RegisterForm = () => {
@@ -11,13 +12,19 @@ export const RegisterForm = () => {
     }
 
     return (
-        <form>
-            <div>
-                <Input />
+        <form className='space-y-12 w-[400px]'>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor='email'>Email</Label>
+                <Input id= 'email' type="email" placeholder="johnsmith@smth.com"/>
             </div>
             
-            <Input />
-            <Button>Register</Button>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor='password'>Password</Label>
+                <Input id= 'password' type="password"/>
+            </div>
+            <div className="w-full">
+                <Button className='w-full bg-green-800 hover:bg-green-400' size="lg">Register</Button>
+            </div>
         </form>
     )
 }
--- grocery-guardian/src/app/register/page.tsx
+++ grocery-guardian/src/app/register/page.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { RegisterForm } from './form'
+import { RegisterForm } from "./form";
+import Link from "next/link";
 
 export default function RegisterPage() {
-    return (
-        <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
-            <div className="shadow-xl p-4 bg-white rounded-xl">
-                <h1>Register</h1>
-                <RegisterForm />
-            </div>
-        </div>
-    )
+  return (
+    <div className="h-screen w-screen flex justify-center items-center bg-green-300">
+      <div className="shadow-xl px-8 py-8 bg-white rounded-xl space-y-12">
+        <h1 className="font-semibold text-2xl">Create your Account</h1>
+        <RegisterForm />
+        <p className="text-center">
+          Have an account?&nbsp;
+          <Link className="text-green-500 hover:underline" href="/login">
+            Sign in
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
