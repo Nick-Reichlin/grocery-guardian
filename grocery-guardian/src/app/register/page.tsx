40a91a0042bd56678a8958a5c9ba9a9febc43ce9
Nick-Reichlin <nickreichlin@gmail.com>
2024-03-08 12:44:39 -0500

    Add first content and styling to register page

---
+++ grocery-guardian/src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function RegisterPage() {
+    return (
+        <div className="h-screen w-screen flex justify-center items-center bg-slate-100">
+            <div className="shadow-xl p-4 bg-white rounded-xl">
+                <h1>Register</h1>
+            </div>
+        </div>
+    )
+}
