01fca8c2e2376bc25530595c0c50739b6dbd5fdd
Nick-Reichlin <nickreichlin@gmail.com>
2024-03-08 23:25:38 -0500

    Finish api register route

---
+++ grocery-guardian/src/app/api/register/route.ts
@@ -0,0 +1,21 @@
+import { hash } from "bcrypt"
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+
+    const {email, password} = await req.json()
+    const hashed = await hash(password, 12)
+
+    const user = await prisma.user.create({
+        data: {
+            email, password: hashed
+        }
+    })
+
+    return NextResponse.json({
+        user: {
+            email: user.email,
+        }
+    })
+}
