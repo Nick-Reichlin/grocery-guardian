cf2b2fb928467f1ab44ef14e6442f1f0986866e3
Nick-Reichlin <nickreichlin@gmail.com>
2024-03-08 23:31:50 -0500

    Add to register/form.tsx required input fields and redirection to login page

--- grocery-guardian/src/app/register/form.tsx
+++ grocery-guardian/src/app/register/form.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { signIn } from "next-auth/react"
 import React from "react"
 
 export const RegisterForm = () => {
@@ -23,7 +24,7 @@ export const RegisterForm = () => {
                 }
             })
             if(res.ok) {
-                // redirect
+                signIn()
             }
         } catch (error) {
             console.error(error)
@@ -37,16 +38,19 @@ export const RegisterForm = () => {
             <div className="grid w-full max-w-sm items-center gap-1.5">
                 <Label htmlFor='email'>Email</Label>
                 <Input
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                id='email' 
-                type="email" 
-                placeholder="johnsmith@smth.com"/>
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    id='email' 
+                    type="email" 
+                    placeholder="johnsmith@smth.com"
+                />
             </div>
             
             <div className="grid w-full max-w-sm items-center gap-1.5">
                 <Label htmlFor='password'>Password</Label>
-                <Input 
+                <Input
+                    required
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     id='password'
