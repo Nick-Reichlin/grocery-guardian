58e201c8faaf91557237629baa9f26f970d97a21
Nick-Reichlin <nickreichlin@gmail.com>
2024-03-09 12:39:24 -0500

    Add form to login page

---
+++ grocery-guardian/src/app/login/form.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Alert } from "@/components/ui/alert"
+import React from "react"
+import { signIn } from "next-auth/react"
+
+export const LoginForm = () => {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const [error, setError] = React.useState<string | null>(null)
+
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        signIn('credentials',{
+            email,
+            password
+        })
+        console.log("Login!")
+    }
+    return(
+        <form onSubmit={onSubmit} className='space-y-12 w-full sm:w-[400px]'>
+            <div className="grid w-full items-center gap-1.5">
+                <Label htmlFor='email'>Email</Label>
+                <Input
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    id='email' 
+                    type="email" 
+                    placeholder="johnsmith@smth.com"
+                />
+            </div>
+            
+            <div className="grid w-full items-center gap-1.5">
+                <Label htmlFor='password'>Password</Label>
+                <Input
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    id='password'
+                    type="password"
+                />
+            </div>
+            {error && <Alert>{error}</Alert>}
+            <div className="w-full">
+                <Button className='w-full bg-green-800 hover:bg-green-400' size="lg">Login</Button>
+            </div>
+        </form>
+)
+}
--- grocery-guardian/src/app/login/page.tsx
+++ grocery-guardian/src/app/login/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import { LoginForm } from './form'
 
 export default function LoginPage() {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-slate-200">
       <div className="sm:shadow-xl px-8 py-8 sm:bg-white rounded-xl space-y-12">
         <h1 className="font-semibold text-2xl">Login</h1>
-        
+        <LoginForm />
         <p className="text-center">
           Need to create an account?&nbsp;
           <Link className="text-green-500 hover:underline" href="/register">
