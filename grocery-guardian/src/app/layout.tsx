18820f21d97b9ab80ab005c230f9aa78e9f42c14
Nick-Reichlin <nickreichlin@gmail.com>
2024-03-11 16:56:43 -0400

    Fix issue with navbar showing up on every page and fixed issue with middleware allowing access to every page

--- grocery-guardian/src/app/layout.tsx
+++ grocery-guardian/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from './providers'
-import Navbar from '@/components/navbar'
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +20,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <Navbar />
           {children}
         </Providers>
       </body>
--- grocery-guardian/src/middleware.ts
+++ grocery-guardian/src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-    matcher: ['/api','/']
+    matcher: ['/','/profile','/inventory','/grocery-lists']
   }
