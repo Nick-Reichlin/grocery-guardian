7cb310ae5a0307546a0b7fe477ed42ba403bb8b4
Nick-Reichlin <nickreichlin@gmail.com>
2024-04-03 18:09:13 -0400

    Start profile form and add it to page

---
+++ grocery-guardian/src/app/profile/form.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Alert } from "@/components/ui/alert"
+import React from "react"
+
+export const ProfileForm = () => {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const [name, setName] = React.useState('')
+    const [error, setError] = React.useState<string | null>(null)
+
+    const onSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+
+        try {
+            const res = await fetch('/api/register', {
+                method: 'POST',
+                body: JSON.stringify({
+                    email, password, name
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if(res.ok) {
+
+            } else {
+                setError((await res.json()).error)
+            }
+        } catch (error: any) {
+            setError(error?.message)
+            console.error(error)
+        }
+    }
+
+    return (
+        <form onSubmit={onSubmit} className='space-y-12 w-full sm:w-[400px]'>
+            <div className="grid w-full items-center gap-1.5">
+                <Label htmlFor='email'>Email</Label>
+                <Input
+                    required
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    id='email' 
+                    type="email" 
+                    placeholder="johnsmith@smth.com"
+                />
+            </div>
+            
+            <div className="grid w-full items-center gap-1.5">
+                <Label htmlFor='password'>Password</Label>
+                <Input
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    id='password'
+                    type="password"
+                />
+            </div>
+
+            <div className="grid w-full items-center gap-1.5">
+                <Label htmlFor='name'>Name</Label>
+                <Input
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    id='name'
+                    type="name"
+                />
+            </div>
+
+            {error && <Alert>{error}</Alert>}
+            <div className="w-full">
+                <Button className='w-full bg-green-800 hover:bg-green-400' size="lg">Update</Button>
+            </div>
+        </form>
+    )
+}
--- grocery-guardian/src/app/profile/page.tsx
+++ grocery-guardian/src/app/profile/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { ProfileForm } from "./form";
 
 export default function RegisterPage() {
   return (
     <div className="h-screen w-screen flex justify-center bg-no-repeat items-center">
       <div className="sm:shadow-xl px-8 py-8 sm:bg-white rounded-xl space-y-12">
-        
+        <ProfileForm/>
       </div>
     </div>
   );
