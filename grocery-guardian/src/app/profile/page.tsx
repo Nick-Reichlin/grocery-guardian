5ba8d170c25f6e2b61292528965e2437e9880e89
Nick-Reichlin <nickreichlin@gmail.com>
2024-04-03 19:28:26 -0400

    Add navbar and bg color to profile page

--- grocery-guardian/src/app/profile/page.tsx
+++ grocery-guardian/src/app/profile/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { ProfileForm } from "./form";
+import Navbar from "@/components/navbar";
 
 export default function RegisterPage() {
   return (
-    <div className="h-screen w-screen flex justify-center bg-no-repeat items-center">
-      <div className="sm:shadow-xl px-8 py-8 sm:bg-white rounded-xl space-y-12">
-        <ProfileForm/>
+    <main>
+      <Navbar />
+      <div className="h-screen w-screen flex justify-center bg-no-repeat items-center bg-emerald-100">
+        <div className="sm:shadow-xl px-8 py-8 sm:bg-white rounded-xl space-y-12">
+          <ProfileForm />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
