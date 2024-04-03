9b88ee248ac6e03123dd7056ebc76b2f4d82dd22
Nick-Reichlin <nickreichlin@gmail.com>
2024-04-03 17:59:02 -0400

    Add base for profile page

---
+++ grocery-guardian/src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function RegisterPage() {
+  return (
+    <div className="h-screen w-screen flex justify-center bg-no-repeat items-center">
+      <div className="sm:shadow-xl px-8 py-8 sm:bg-white rounded-xl space-y-12">
+        
+      </div>
+    </div>
+  );
+}
