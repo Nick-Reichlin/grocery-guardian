6459c1b3bf8e9cc7ef0525f95c04155106914462
Nick-Reichlin <nickreichlin@gmail.com>
2024-04-19 15:33:03 -0400

    Improve appearance of receipt upload page

--- grocery-guardian/src/app/upload-receipt/page.tsx
+++ grocery-guardian/src/app/upload-receipt/page.tsx
@@ -30,21 +30,27 @@ export default function ReceiptUpload() {
     return (
         <main>
             <Navbar />
-            <div className="h-screen w-screen flex justify-center bg-no-repeat items-center bg-emerald-100">
+            <div className="h-screen w-screen flex justify-center items-center bg-no-repeat bg-emerald-100">
                 <div className="w-full h-full sm:shadow-xl bg-white rounded-xl p-8 mt-20">
                     <h1 className="text-3xl font-bold text-center text-gray-800 mb-8 pb-2">
                         Receipt Upload
                     </h1>
-                    <form onSubmit={onSubmit}>
-                        <input 
+                    <form onSubmit={onSubmit} className="flex flex-col items-center">
+                        <input
                             type="file"
-                            name ="file"
+                            name="file"
                             onChange={(e) => setFile(e.target.files?.[0])}
+                            className="mb-4"
                         />
-                        <input type="submit" value="Upload" />
+                        <button
+                            type="submit"
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                            Upload
+                        </button>
                     </form>
                 </div>
             </div>
-        </main>
+</main>
     );
 }
