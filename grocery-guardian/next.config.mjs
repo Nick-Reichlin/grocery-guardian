5a29ffb5c8d330d36c3f01543559e18618518b3b
Nick-Reichlin <nickreichlin@gmail.com>
2024-04-19 12:51:03 -0400

    Add api route

--- grocery-guardian/next.config.mjs
+++ grocery-guardian/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        serverComponentExternalPackages: ['pdf2json'],
-    }
-};
-
-export default nextConfig;
+      serverComponentsExternalPackages: ['pdf2json'],
+    },
+  };
+  
+module.exports = nextConfig;
