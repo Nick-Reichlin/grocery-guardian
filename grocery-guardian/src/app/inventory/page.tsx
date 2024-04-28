46d9370af489d4bae09b0cc690e4ade81a307a17
Nick-Reichlin <nickreichlin@gmail.com>
2024-04-28 16:54:40 -0400

    Add extentibility to inventory page and change styling

--- grocery-guardian/src/app/inventory/page.tsx
+++ grocery-guardian/src/app/inventory/page.tsx
@@ -1,20 +1,19 @@
-
+import React from 'react';
 import Navbar from "@/components/navbar";
 import { InventoryForm } from "./form";
 import InvenetoryTable from "@/components/inventoryTable";
 
-export default async function InventoryPage() {
-
+export default function InventoryPage() {
   return (
     <main>
       <Navbar />
-      <div className="h-screen w-screen flex justify-center bg-no-repeat items-center bg-emerald-100">
-        <div className="w-full h-full sm:shadow-xl bg-white rounded-xl p-8 mt-20">
-            <h1 className="text-3xl font-bold text-center text-gray-800 mb-8 pb-2">
-            Your Groceries
+      <div className="flex justify-center items-start bg-no-repeat bg-emerald-100 min-h-screen w-full">
+        <div className="w-full bg-white rounded-xl p-8 my-5 mx-auto shadow-xl overflow-hidden">
+            <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
+              Your Groceries
             </h1>
-            <InventoryForm/>
-            <InvenetoryTable/>
+            <InventoryForm />
+            <InvenetoryTable />
         </div>
       </div>
     </main>
